ad6007e36d144cadf3e815bfe685e37815fcdc32
Mazepa <mfmazepa@hotmail.com>
2018-11-29 21:43:31 -0200

    Datas pelo carbon

--- project/app/Http/Controllers/GameController.php
+++ project/app/Http/Controllers/GameController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 use App\Model\Address;
 use App\Model\Game;
 use App\Model\GameCategory;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class GameController extends Controller
@@ -31,6 +32,9 @@ class GameController extends Controller
         $address = new Address();
         $gameCategory = (new GameCategory())->where('name', $data['game_category'])->first();
         $address = $address->create( $request->only([ 'street', 'district', 'city', 'state',]));
+        $data['start_time'] = Carbon::createFromFormat('hh:mm', $data['start_time']);
+        $data['end_time'] = Carbon::createFromFormat('hh:mm', $data['end_time']);
+        $data['date'] = Carbon::createFromFormat('Y-m-d', $data['end_time']);
         $data['address_id'] = $address->id;
         $data['game_category_id'] = $gameCategory->id;
         $game->create($data);
